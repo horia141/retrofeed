295fafb552a5fd1cbee24b1c90e85d6bd3deca07
Horia Coman <horia141@gmail.com>
2018-07-01 23:22:13 +0300

    Add lint support

--- test/index-test.ts
+++ test/index-test.ts
@@ -1,10 +1,10 @@
-import 'mocha';
-import { expect } from 'chai';
+import { expect } from "chai";
+import "mocha";
 
-import { addTwo } from '../src/index';
+import { addTwo } from "../src/index";
 
-describe('addTwo', () => {
-    it('Should add 2', () => {
+describe("addTwo", () => {
+    it("Should add 2", () => {
         expect(addTwo(10)).to.eql(12);
     });
 });
