f5454e95bc35e2c91a092da876e4892d5471a392
Horia Coman <horia141@gmail.com>
2018-07-12 23:35:38 +0300

    Try with s

--- test-e2e/index.ts
+++ test-e2e/index.ts
@@ -1,6 +1,10 @@
 import "cypress";
 import "mocha";
 
+import * as os from "os";
+
+console.log(os.homedir());
+
 describe("Main page", () => {
     it("Has main data", () => {
         cy.visit("/");
