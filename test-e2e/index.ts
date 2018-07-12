db2bfe529d71d263bec162f2704405b9d4eb5b72
Horia Coman <horia141@gmail.com>
2018-07-12 23:35:52 +0300

    Try without docker containers

--- test-e2e/index.ts
+++ test-e2e/index.ts
@@ -1,10 +1,6 @@
 import "cypress";
 import "mocha";
 
-import * as os from "os";
-
-console.log(os.homedir());
-
 describe("Main page", () => {
     it("Has main data", () => {
         cy.visit("/");
